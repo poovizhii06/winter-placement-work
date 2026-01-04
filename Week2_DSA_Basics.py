26b018cb3e6497dcd39aaa05d153b0900df8d9a6
poovizhii06 <ps1706@srmist.edu.in>
2026-01-04 23:05:41 +0530

    Create Week2_DSA_Basics.py

---
+++ Week2_DSA_Basics.py
@@ -0,0 +1,17 @@
+# Week 2 - DSA Basics
+
+# List operations
+arr = [10, 20, 30, 40]
+arr.append(50)
+print(arr)
+
+# Search element
+key = 30
+if key in arr:
+    print("Found")
+else:
+    print("Not Found")
+
+# Simple sorting
+arr.sort()
+print("Sorted list:", arr)
